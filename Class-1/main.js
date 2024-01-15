d677a94e393edc1cbd56df8121c482320eb3693e
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-01-15 20:39:16 +0530

    Simple If

--- Class-1/main.js
+++ Class-1/main.js
@@ -1,3 +1,12 @@
 let firstName = "Atheeq"
 let age = "20"
+let isPro = true;
 console.log("This person name is " + firstName + " and his age is " + age);
+if(isPro == true)
+{
+    console.log("Atheeq is Pro");
+}
+else
+{
+    console.log("Atheeq is not Pro");
+}
